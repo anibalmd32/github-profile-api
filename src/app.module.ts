1304c27c81a0b66c3124b1588ef9c9446c8807a1
Anibal Mendoza <104891242+anibalmd32@users.noreply.github.com>
2026-07-24 10:37:38 -0400

    feat: install config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: '.env',
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
